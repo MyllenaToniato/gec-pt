933cae7dedc1a454b6fd25f43206529e99057e44
Akilanny Silva Cruz <souphlannn@gmail.com>
2024-09-28 17:22:45 -0300

    Leitura.py

--- Leitura.py
+++ Leitura.py
@@ -1,15 +1,17 @@
-
 from nltk.tokenize import sent_tokenize, word_tokenize
 import os
 import xml.etree.ElementTree as ET
+
 #-------------------------------------------------
 
 def verificar_caminho(caminho_base):
-    #Verifica se o caminho fornecido pelo usuário existe
+    #Verifica se o caminho fornecido pelo usuário existe.
     if not os.path.exists(caminho_base):
         print(f'O caminho {caminho_base} não existe.')
         return False
     return True
+
+
 
 def ler_arquivo_xml(caminho_arquivo):
     #Lê um arquivo XML e retorna o corpo do XML se existir.
@@ -21,6 +23,8 @@ def ler_arquivo_xml(caminho_arquivo):
     except ET.ParseError as e:
         print(f'Erro ao ler o arquivo {caminho_arquivo}: {e}')
         return None
+    
+
 
 def extrair_sentencas(body):
     #Extrai as sentenças 'wrong' e 'correct' de um corpo XML.
@@ -38,8 +42,22 @@ def extrair_sentencas(body):
 
     return wrong_sentences, correct_sentences
 
+
+
+
+def imprimir_body(body, caminho_arquivo):
+    #Imprime o conteúdo do body do arquivo XML
+    if body is not None:
+        body_content = ET.tostring(body, encoding='utf-8').decode('utf-8')
+        print(f"Conteúdo do <body> do arquivo '{caminho_arquivo}':")
+        print(body_content)
+    else:
+        print(f"O arquivo '{caminho_arquivo}' não contém um <body>.")
+
+
+
 def percorrer_pastas(caminho_base):
-    #Percorre as pastas e arquivos XML no caminho base e extrai sentenças.
+    #Percorre as pastas e arquivos XML no caminho base e extrai sentenças
     wrong_sentences_total = []
     correct_sentences_total = []
 
@@ -50,25 +68,30 @@ def percorrer_pastas(caminho_base):
                 body = ler_arquivo_xml(caminho_arquivo)
 
                 if body is not None:
+                    # Imprime o conteúdo do body diretamente ao ler o arquivo
+                    imprimir_body(body, caminho_arquivo)
+
+                    # Extrai e acumula as sentenças 'wrong' e 'correct'
                     wrong_sentences, correct_sentences = extrair_sentencas(body)
                     wrong_sentences_total.extend(wrong_sentences)
                     correct_sentences_total.extend(correct_sentences)
 
     return wrong_sentences_total, correct_sentences_total
 
+
+
 def main():
-    # Solicita o caminho base ao usuário
+    
+    # Solicita o caminho base ao 
     caminho_base = input("Digite o caminho base para percorrer as pastas: ")
 
-    
+    # Verifica se o caminho é válido
     if verificar_caminho(caminho_base):
-       
         wrong_sentences, correct_sentences = percorrer_pastas(caminho_base)
 
-        
+        # Imprime sentenças incorretas e corretas
         print("Sentenças incorretas:", wrong_sentences)
         print("Sentenças corretas:", correct_sentences)
-
 
 if __name__ == "__main__":
     main()
