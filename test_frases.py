4a8d96c946cd8497d66b46baada696a5b02809c4
Evelin Amorim <evelin.f.amorim@inescted.pt>
2024-10-15 14:35:44 +0100

    1. arquivo de requirements 2. Teste da função que dada uma lista de frases com erros, retorna as mesmas no formato BIO 3. Arquivo sample.txt que contém um exemplo de frases no formato BIO

--- test_frases.py
+++ test_frases.py
@@ -24,6 +24,40 @@ class TestCaseFrases(unittest.TestCase):
                           "Com a efemeridade dos relacionamentos atuais, há casos<wrong>onde</wrong><correct>em que</correct>a pessoa tem relações com muitas outras, espalhando a doença antes de descobrir que a tem."]
         self.assertEqual(sentencas, sentencas_alvo)  # add assertion here
 
+    def test_frases_BIO(self):
+        body = """
+                De fato, estes não curam a<wrong>doença</wrong>
+                <correct>doença,</correct>
+                apenas retardam seus danos e,
+                 mesmo com o seu uso, a AIDS é muito devastadora e a própria química forte dos remédios tem efeitos negativos sobre o organismo, mas em escala muito menor que a enfermidade. Mesmo com esses problemas, a banalização da doença é frequente e gera irresponsabilidades, como não usar preservativos. A principal desculpa para não<wrong>usar-se</wrong>
+                <correct>se usar</correct>
+                camisinha é a confiança no parceiro, porém, ele pode ter contraído o HIV anteriormente e não saber, pois, às vezes, o vírus permanece no corpo por anos antes
+                <wrong>da</wrong>
+                <correct>de a</correct>
+                doença se manifestar. Com a efemeridade dos relacionamentos atuais, há casos
+                <wrong>onde</wrong>
+                <correct>em que</correct>
+                a pessoa tem relações com muitas outras, espalhando a doença antes de descobrir que a tem. Portanto, o fato de conhecer o outro e a aparência saudável deste não fazem uma relação sexual ser necessariamente segura.
+                 Enquanto a ciência, que tem trabalhado arduamente, não encontra a cura e a vacina da Aids é fundamental alertar a população, por meio de campanhas na mídia e nas escolas, a fim de impedir que o descaso e a falta de informação espalhem mais ainda esse mal.
+                """
+        with open("data/sample.txt","r") as fd:
+            answer = []
+            sent = []
+            for line in fd:
+                items = line.strip().split()
+                if len(items) > 0:
+                    token = items[0]
+                    tag = items[1]
+                    sent.append((token,tag))
+                else:
+                    answer.append(sent)
+            answer.append(sent)
+
+        sentencas = extrair_sentencas(body)
+        sentencas_BIO = sent2BIO(sentencas)
+        self.assertEqual(sentencas_BIO, answer)
+
+
 
 if __name__ == '__main__':
     unittest.main()
